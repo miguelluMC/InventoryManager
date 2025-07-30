7fc48ca7d5781987010fd73bf41d77bedde2da57
miguelluLC <miguelalazacazares@hotmail.com>
2025-07-29 19:41:16 -0600

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventoryManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenProductsModule(object sender, RoutedEventArgs e)
+        {
+            // Abrir ventana de gestión de productos
+        }
+
+        private void OpenInventoryModule(object sender, RoutedEventArgs e)
+        {
+            // Abrir ventana de control de inventario
+        }
+
+        private void OpenSuppliersModule(object sender, RoutedEventArgs e)
+        {
+            // Abrir ventana de control de inventario
+        }
+
+        private void OpenPurchaseOrdersModule(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenCategoriesModule(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenReportsModule(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void OpenSettingsModule(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void OpenMovementsModule(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
